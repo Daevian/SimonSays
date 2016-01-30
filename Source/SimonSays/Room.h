b85ef4d9719bdd57c64c7f86485baf4dda9ae1d4
Daevian <daevian@gmail.com>
2016-01-30 09:40:04 +0900

    added room class

---
+++ Source/SimonSays/Room.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "PaperSpriteActor.h"
+#include "Room.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMONSAYS_API ARoom : public APaperSpriteActor
+{
+    GENERATED_BODY()
+    
+public:
+    ARoom();
+    ~ARoom();
+
+
+    
+    
+    
+};
